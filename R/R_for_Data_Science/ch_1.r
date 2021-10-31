fc9316d28d244820f8d6e29e564c7675e8b71b14
cristian <cmluna2913@gmail.com>
2021-10-31 00:21:58 -0500

    reading Ch 1

--- R/R_for_Data_Science/ch_1.r
+++ R/R_for_Data_Science/ch_1.r
@@ -1,9 +1,50 @@
-# graph with ggplot
-ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+# import library
+library(tidyverse)
 
-"snippet 1": {
-    library(tidyverse)
-    myplt <- ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
-    print(myplt)
-    print("hello world")
+# graph with ggplot
+# ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+
+# make cell block
+"name for block"{
+  # code
 }
+
+"code block 1": {
+  myplt <- ggplot(data = mpg) + geom_point(mapping = aes(x = displ, 
+                                                         y = hwy,
+                                                         color = class))
+  print(myplt)
+}
+
+"visualize by class size": {
+  myplt <- ggplot(data = mpg) + geom_point(mapping = aes(x = displ,
+                                                         y = hwy,
+                                                         size = class))
+  print(myplt)
+}
+
+"visualize by transparency": {
+  myplt <- ggplot(data = mpg) + geom_point(mapping = aes(x = displ,
+                                                         y = hwy,
+                                                         alpha = class))
+  print(myplt)
+}
+
+"visualize by shape": {
+  myplt <- ggplot(data = mpg) + geom_point(mapping = aes(x = displ,
+                                                         y = hwy,
+                                                         shape = class))
+  print(myplt)
+}
+
+"set properties for geom_point": {
+  myplt <- ggplot(data=mpg) + geom_point(mapping = aes(x = displ,
+                                                       y = hwy),
+                                         color = "blue")
+  print(myplt)
+}
+
+# check a variable dtype
+# class(dataframe$variable)
+class(mpg$hwy)
+
