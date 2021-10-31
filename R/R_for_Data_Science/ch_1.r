b8c243d8e9e09907f8465e60aa16e06fe0559f02
cristian <cmluna2913@gmail.com>
2021-10-31 01:11:50 -0500

    reading ch 1

--- R/R_for_Data_Science/ch_1.r
+++ R/R_for_Data_Science/ch_1.r
@@ -48,3 +48,33 @@ library(tidyverse)
 # class(dataframe$variable)
 class(mpg$hwy)
 
+"set properties for geom_point": {
+  myplt <- ggplot(data=mpg) + geom_point(mapping = aes(x = displ,
+                                                       y = hwy),
+                                         color = "blue")
+  print(myplt)
+}
+
+# check for factor dtype
+factors <- sapply(mpg, is.factor)
+which(factors)
+
+# check for numeric dtype
+numerics <- sapply(mpg, is.numeric)
+which(numerics)
+
+# check for integer dtype
+integers <- sapply(mpg,is.integer)
+which(integers)
+
+"use continuous variables": {
+  myplt <- ggplot(data=mpg) + geom_point(mapping = aes(x = hwy,
+                                                       y = hwy),
+                                         color = "blue",
+                                         stroke = 1)
+  print(myplt)
+}
+
+# pull up documentation for a method
+?geom_point
+
